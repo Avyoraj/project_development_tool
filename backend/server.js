d1ae8fd5ec80315054bf9198bf4dfd17c42d755b
Harshwardhan Vishwakarma <hvishwakarma1729@gmail.com>
2025-03-02 16:57:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use(cors({
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.use(api)
+app.use('/api', api);
 
 app.listen(PORT, () => {
     console.log(`Your app is running in http://localhost:${PORT}`)
